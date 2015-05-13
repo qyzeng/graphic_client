1afce2193306b08c1834ac2487f6a12e4f85c7d1
weiping <workaholic.angel.86@gmail.com>
2015-05-14 00:48:51 +0800

    Reorganized some files and created new Isosurface scene

---
+++ Assets/Scripts/IsosurfaceCreator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsosurfaceCreator : MonoBehaviour
+{
+
+	private GameObject mReferenceGameObject = null;
+	public GameObject IsosurfaceObject {
+		get {
+			if (mReferenceGameObject == null) {
+				mReferenceGameObject = new GameObject ("IsosurfaceObject");
+			}
+			return mReferenceGameObject;
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
