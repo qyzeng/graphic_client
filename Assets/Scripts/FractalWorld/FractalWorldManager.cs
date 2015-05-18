b99eae1f4e0c42612bfcb8f9e142016848b608e5
weiping <workaholic.angel.86@gmail.com>
2015-05-18 18:29:27 +0800

    Linked Keyboard to State Machine

--- Assets/Scripts/FractalWorld/FractalWorldManager.cs
+++ Assets/Scripts/FractalWorld/FractalWorldManager.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FractalWorldManager : MonoBehaviour {
+public class FractalWorldManager : MonoBehaviour
+{
+
+	public CharacterStateMachine Player;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		if (Player != null) {
+			Player.AddController (WP.Controller.StandalonePlayerController.Singleton);
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
