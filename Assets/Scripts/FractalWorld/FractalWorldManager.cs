2cd0440403b85dd90476989486e828f6a8bbb0d3
weiping <workaholic.angel.86@gmail.com>
2015-05-21 08:36:17 +0800

    WIP: Fractal world

--- Assets/Scripts/FractalWorld/FractalWorldManager.cs
+++ Assets/Scripts/FractalWorld/FractalWorldManager.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using FractalLibrary;
 
 public class FractalWorldManager : MonoBehaviour
 {
+	MandelbrotFractal mMandelbrotfractal = new MandelbrotFractal ();
 
 	public CharacterStateMachine Player;
 
@@ -12,6 +14,12 @@ public class FractalWorldManager : MonoBehaviour
 		if (Player != null) {
 			Player.AddController (WP.Controller.StandalonePlayerController.Singleton);
 		}
+	}
+
+	private void InitFractal ()
+	{
+		mMandelbrotfractal.Iterations = 100;
+		mMandelbrotfractal.SetCenter (0, 0);
 	}
 	
 	// Update is called once per frame
